6684385c43e06f085b3283aa3d068d1d5658ceca
Atibhav Mittal <atibhav@s-169-232-81-215.resnet.ucla.edu>
2015-11-28 21:26:12 -0800

    Initial Commit

---
+++ project7/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  project7
+//
+//  Created by Atibhav Mittal on 11/28/15.
+//  Copyright © 2015 ati. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
